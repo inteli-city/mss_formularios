68e96e9321012aa33b23cda0a5a54c5d200678af
Gabriel de Godoy Braz <61150821+GabrielGodoy01@users.noreply.github.com>
2024-07-23 13:24:55 -0300

    bucket_name

--- iac/iac/iac_stack.py
+++ iac/iac/iac_stack.py
@@ -22,6 +22,7 @@ class IacStack(Stack):
         self.user_pool_id = os.environ.get("USER_POOL_ID")
         self.app_client_id = os.environ.get("APP_CLIENT_ID")
         self.github_ref_name = os.environ.get("GITHUB_REF_NAME")
+        self.bucket_name = os.environ.get("BUCKET_NAME")
 
         self.dynamo_stack = DynamoStack(self)
 
@@ -52,6 +53,7 @@ class IacStack(Stack):
             "DYNAMO_PARTITION_KEY": "PK",
             "DYNAMO_SORT_KEY": "SK",
             "REGION": self.region,
+            "BUCKET_NAME": self.bucket_name,
         }
 
         api_gateway_resource = self.rest_api.root.add_resource("mss-formularios", default_cors_preflight_options={
